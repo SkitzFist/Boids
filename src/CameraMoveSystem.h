cbb3d23c87297a6fe9b6c69ddb4508f84856b72c
SkitzFist <48617943+SkitzFist@users.noreply.github.com>
2024-11-22 23:18:53 +0100

    Add Simple camera movement (#1)

---
+++ src/CameraMoveSystem.h
@@ -0,0 +1,10 @@
+#ifndef BOIDS_CAMERA_MOVE_SYSTEM_H
+#define BOIDS_CAMERA_MOVE_SYSTEM_H
+
+#include "raylib.h"
+
+void updateCamera(Camera2D& camera, const float dt);
+void handleMove(Camera2D& camera, const float dt);
+void handleZoom(Camera2D& camera, const float dt);
+
+#endif
--- src/Simulation.h
+++ src/Simulation.h
@@ -1,6 +1,8 @@
 #ifndef BOIDS_SIMULATION_H
 #define BOIDS_SIMULATION_H
 
+#include "raylib.h"
+
 class Simulation {
   public:
     Simulation();
@@ -12,10 +14,16 @@ class Simulation {
     void onResize(int width, int height);
 
   private:
+    Camera2D m_camera;
+    Rectangle m_cameraRect;
+
+  private:
     void gameLoop();
     void handleInput();
     void update(float dt);
     void render() const;
+
+    Rectangle getCameraRect() const;
 };
 
 #endif
