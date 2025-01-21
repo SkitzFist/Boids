7576bdf5e21ef09f76ffe19fc25b6af3934ed90a
SkitzFist <jo.persson1@hotmail.se>
2025-01-21 22:42:02 +0100

    Simd sorting: update comment

--- src/SimdSorting.h
+++ src/SimdSorting.h
@@ -6,10 +6,11 @@
 #include <vector>
 
 /*
-    I have a gut feeling it's possible to simd sort a register in 5-6 steps.
-    Which is less then what known mathematics say. I've spent two weeks on this now,
-    but havn't solved it yet, it feels like I'm getting closer though.
-    But I'll have to leave it for now and return on a later date.
+    Ok I managed to solve this in 6 steps, not sure if it's possible to go lower.
+    Anyway, this type of sorting won't be of use in this project, unless I can crack
+    how to certainly store what index swaps is happening so it can be reflected in the
+    entityId array.
+    This only sorts on register basis, need to figure out how to sort two registers with eachother.
     TODO: Move this out to it's own repo.
 */
 
@@ -33,34 +34,8 @@ constexpr int createMask(int a, int b, int c, int d, int e, int f, int g, int h)
            (h ? 1 : 0) << 7;
 }
 
-inline void doStep(__m256i& a, __m256i& b, __m256i& idx, int& blend) {
-    b = _mm256_permutevar8x32_epi32(a, idx);
-    __m256i min = _mm256_min_epi32(a, b);
-    __m256i max = _mm256_max_epi32(a, b);
-    a = _mm256_blend_epi32(min, max, blend);
-}
-
-/*
-    __m256i idxPairOdd = _mm256_set_epi32(7, 5, 6, 3, 4, 1, 2, 0);
-    constexpr const int blendPairOdd = createMask(0, 0, 1, 0, 1, 0, 1, 1);
-*/
-
 __m256i sortRegister(__m256i reg) {
     // print_m256i(reg);
-
-    /*
-    A: [(0,2),(1,3),(4,6),(5,7)]
-
-    B: [(0,4),(1,5),(2,6),(3,7)]
-
-    C: [(0,1),(2,3),(4,5),(6,7)]
-
-    D: [(2,4),(3,5)]
-
-    E: [(1,4),(3,6)]
-
-    F: [(1,2),(3,4),(5,6)]
-    */
 
     __m256i idxA = _mm256_set_epi32(5, 4, 7, 6, 1, 0, 3, 2);
     constexpr int blendA = createMask(0, 0, 1, 1, 0, 0, 1, 1);
